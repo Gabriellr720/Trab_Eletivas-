2ad4d42365a958fd02722f78572b7fd784785b96
Gabriellr720 <glimarocha662@gmail.com>
2025-10-28 17:56:08 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    
+?>
+
+<!DOCTYbuttonE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <header>
+        <h1>The Gastronomy</h1>
+    </header>
+
+    <main>
+        <section class="f2">
+            <section class="f1"> 
+                <h3>Cadastro </h3>
+                <form>
+                    <input type="text" id="email" placeholder="Email" name="email">
+                    <input type="password" id="password" placeholder="Senha" name="password">
+                   
+                </form>
+                <section class="f4">
+                    <button class="btn-login" type="submit"><a href="view/Home.HTML">ENTRE</a></button>
+                </section>
+            </section>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+,</body>
+</html>
