8ab3ffe814e286fc5ffddc2df754493649ae4550
Gabriellr720 <glimarocha662@gmail.com>
2025-11-13 18:43:27 -0300

    Renomeação do arquivo SQL

--- trab_eletivas (1).sql
+++ trab_eletivas (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Nov-2025 às 19:58
+-- Tempo de geração: 13-Nov-2025 às 22:34
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.0.30
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `trab_eletivas`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `inserecomentario`
+--
+
+CREATE TABLE `inserecomentario` (
+  `id_comentario` int(10) NOT NULL,
+  `comentario` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `inserecomentario`
+--
+
+INSERT INTO `inserecomentario` (`id_comentario`, `comentario`) VALUES
+(1, 'Sallllve '),
+(2, 'Sallllve '),
+(3, 'jwbvsbv');
 
 -- --------------------------------------------------------
 
@@ -42,6 +62,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`cpf`, `nome`, `sobrenome`, `telefone`, `email`, `senha`, `dataNasc`) VALUES
+('091487314', 'wnjbvwbvou', 'wnvwp vo', '51651884', 'fwebg wbg', 'wkjg bg', '2008-02-10'),
 ('1234567', 'julio', 'cleber', '1899999999', 'cleber@gmail.com ', '123456', '2004-08-05'),
 ('321654', 'rojer', 'opejtjjt', '878545165', 'sgwghethrj', 'w484g 4e', '2025-11-04'),
 ('87/54651', 'sagwg', 'tjrthjaehj', '467561614', 'ghaeheh', '4541987987', '2025-10-30');
@@ -51,10 +72,26 @@ INSERT INTO `usuario` (`cpf`, `nome`, `sobrenome`, `telefone`, `email`, `senha`,
 --
 
 --
+-- Índices para tabela `inserecomentario`
+--
+ALTER TABLE `inserecomentario`
+  ADD PRIMARY KEY (`id_comentario`);
+
+--
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`cpf`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `inserecomentario`
+--
+ALTER TABLE `inserecomentario`
+  MODIFY `id_comentario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
