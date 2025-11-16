f45bb2604af5846d9707bd313fdf73dc905f8557
Gabriellr720 <glimarocha662@gmail.com>
2025-11-16 14:04:33 -0300

    Resolvendo conflito de merge nas funções de cliente

--- processamento/cadastrarComentario.php
+++ processamento/cadastrarComentario.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../assets/img/favicon.png">
     <!--<link rel="stylesheet" href="../assets/css/cadastrarComentario.css">-->
-    <link rel="stylesheet" href="./assets/css/cadastrarComentarios.css">
+    <link rel="stylesheet" href="../assets/css/cadastrarComentario.css">
     <title>Cadastrar Comentário </title>
 </head>
 <body>
